52c8aef3a1cafc2e50a658a0aed2475b19cb59c6
Pascal Grimaud <pascalgrimaud@gmail.com>
2019-03-01 08:09:10 +0100

    Add entity country

---
+++ src/main/java/io/github/pascalgrimaud/referential/domain/Country.java
@@ -0,0 +1,165 @@
+package io.github.pascalgrimaud.referential.domain;
+
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Country.
+ */
+@Entity
+@Table(name = "country")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Country implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "code_alpha_2", nullable = false)
+    private String codeAlpha2;
+
+    @NotNull
+    @Column(name = "code_alpha_3", nullable = false)
+    private String codeAlpha3;
+
+    @Column(name = "continent")
+    private String continent;
+
+    @Column(name = "indicatif")
+    private String indicatif;
+
+    @Column(name = "source")
+    private String source;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Country name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCodeAlpha2() {
+        return codeAlpha2;
+    }
+
+    public Country codeAlpha2(String codeAlpha2) {
+        this.codeAlpha2 = codeAlpha2;
+        return this;
+    }
+
+    public void setCodeAlpha2(String codeAlpha2) {
+        this.codeAlpha2 = codeAlpha2;
+    }
+
+    public String getCodeAlpha3() {
+        return codeAlpha3;
+    }
+
+    public Country codeAlpha3(String codeAlpha3) {
+        this.codeAlpha3 = codeAlpha3;
+        return this;
+    }
+
+    public void setCodeAlpha3(String codeAlpha3) {
+        this.codeAlpha3 = codeAlpha3;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public Country continent(String continent) {
+        this.continent = continent;
+        return this;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getIndicatif() {
+        return indicatif;
+    }
+
+    public Country indicatif(String indicatif) {
+        this.indicatif = indicatif;
+        return this;
+    }
+
+    public void setIndicatif(String indicatif) {
+        this.indicatif = indicatif;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public Country source(String source) {
+        this.source = source;
+        return this;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Country)) {
+            return false;
+        }
+        return id != null && id.equals(((Country) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", codeAlpha2='" + getCodeAlpha2() + "'" +
+            ", codeAlpha3='" + getCodeAlpha3() + "'" +
+            ", continent='" + getContinent() + "'" +
+            ", indicatif='" + getIndicatif() + "'" +
+            ", source='" + getSource() + "'" +
+            "}";
+    }
+}
---
+++ src/test/java/io/github/pascalgrimaud/referential/web/rest/CountryResourceIT.java
@@ -0,0 +1,348 @@
+package io.github.pascalgrimaud.referential.web.rest;
+
+import io.github.pascalgrimaud.referential.ReferentialApp;
+
+import io.github.pascalgrimaud.referential.domain.Country;
+import io.github.pascalgrimaud.referential.repository.CountryRepository;
+import io.github.pascalgrimaud.referential.service.CountryService;
+import io.github.pascalgrimaud.referential.web.rest.errors.ExceptionTranslator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.Validator;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+
+import static io.github.pascalgrimaud.referential.web.rest.TestUtil.createFormattingConversionService;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Integration tests for the {@Link CountryResource} REST controller.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ReferentialApp.class)
+public class CountryResourceIT {
+
+    private static final String DEFAULT_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_NAME = "BBBBBBBBBB";
+
+    private static final String DEFAULT_CODE_ALPHA_2 = "AAAAAAAAAA";
+    private static final String UPDATED_CODE_ALPHA_2 = "BBBBBBBBBB";
+
+    private static final String DEFAULT_CODE_ALPHA_3 = "AAAAAAAAAA";
+    private static final String UPDATED_CODE_ALPHA_3 = "BBBBBBBBBB";
+
+    private static final String DEFAULT_CONTINENT = "AAAAAAAAAA";
+    private static final String UPDATED_CONTINENT = "BBBBBBBBBB";
+
+    private static final String DEFAULT_INDICATIF = "AAAAAAAAAA";
+    private static final String UPDATED_INDICATIF = "BBBBBBBBBB";
+
+    private static final String DEFAULT_SOURCE = "AAAAAAAAAA";
+    private static final String UPDATED_SOURCE = "BBBBBBBBBB";
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Autowired
+    private CountryService countryService;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private Validator validator;
+
+    private MockMvc restCountryMockMvc;
+
+    private Country country;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        final CountryResource countryResource = new CountryResource(countryService);
+        this.restCountryMockMvc = MockMvcBuilders.standaloneSetup(countryResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setConversionService(createFormattingConversionService())
+            .setMessageConverters(jacksonMessageConverter)
+            .setValidator(validator).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Country createEntity(EntityManager em) {
+        Country country = new Country()
+            .name(DEFAULT_NAME)
+            .codeAlpha2(DEFAULT_CODE_ALPHA_2)
+            .codeAlpha3(DEFAULT_CODE_ALPHA_3)
+            .continent(DEFAULT_CONTINENT)
+            .indicatif(DEFAULT_INDICATIF)
+            .source(DEFAULT_SOURCE);
+        return country;
+    }
+
+    @Before
+    public void initTest() {
+        country = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createCountry() throws Exception {
+        int databaseSizeBeforeCreate = countryRepository.findAll().size();
+
+        // Create the Country
+        restCountryMockMvc.perform(post("/api/countries")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(country)))
+            .andExpect(status().isCreated());
+
+        // Validate the Country in the database
+        List<Country> countryList = countryRepository.findAll();
+        assertThat(countryList).hasSize(databaseSizeBeforeCreate + 1);
+        Country testCountry = countryList.get(countryList.size() - 1);
+        assertThat(testCountry.getName()).isEqualTo(DEFAULT_NAME);
+        assertThat(testCountry.getCodeAlpha2()).isEqualTo(DEFAULT_CODE_ALPHA_2);
+        assertThat(testCountry.getCodeAlpha3()).isEqualTo(DEFAULT_CODE_ALPHA_3);
+        assertThat(testCountry.getContinent()).isEqualTo(DEFAULT_CONTINENT);
+        assertThat(testCountry.getIndicatif()).isEqualTo(DEFAULT_INDICATIF);
+        assertThat(testCountry.getSource()).isEqualTo(DEFAULT_SOURCE);
+    }
+
+    @Test
+    @Transactional
+    public void createCountryWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = countryRepository.findAll().size();
+
+        // Create the Country with an existing ID
+        country.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restCountryMockMvc.perform(post("/api/countries")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(country)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Country in the database
+        List<Country> countryList = countryRepository.findAll();
+        assertThat(countryList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    public void checkNameIsRequired() throws Exception {
+        int databaseSizeBeforeTest = countryRepository.findAll().size();
+        // set the field null
+        country.setName(null);
+
+        // Create the Country, which fails.
+
+        restCountryMockMvc.perform(post("/api/countries")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(country)))
+            .andExpect(status().isBadRequest());
+
+        List<Country> countryList = countryRepository.findAll();
+        assertThat(countryList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void checkCodeAlpha2IsRequired() throws Exception {
+        int databaseSizeBeforeTest = countryRepository.findAll().size();
+        // set the field null
+        country.setCodeAlpha2(null);
+
+        // Create the Country, which fails.
+
+        restCountryMockMvc.perform(post("/api/countries")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(country)))
+            .andExpect(status().isBadRequest());
+
+        List<Country> countryList = countryRepository.findAll();
+        assertThat(countryList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void checkCodeAlpha3IsRequired() throws Exception {
+        int databaseSizeBeforeTest = countryRepository.findAll().size();
+        // set the field null
+        country.setCodeAlpha3(null);
+
+        // Create the Country, which fails.
+
+        restCountryMockMvc.perform(post("/api/countries")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(country)))
+            .andExpect(status().isBadRequest());
+
+        List<Country> countryList = countryRepository.findAll();
+        assertThat(countryList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void getAllCountries() throws Exception {
+        // Initialize the database
+        countryRepository.saveAndFlush(country);
+
+        // Get all the countryList
+        restCountryMockMvc.perform(get("/api/countries?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(country.getId().intValue())))
+            .andExpect(jsonPath("$.[*].name").value(hasItem(DEFAULT_NAME.toString())))
+            .andExpect(jsonPath("$.[*].codeAlpha2").value(hasItem(DEFAULT_CODE_ALPHA_2.toString())))
+            .andExpect(jsonPath("$.[*].codeAlpha3").value(hasItem(DEFAULT_CODE_ALPHA_3.toString())))
+            .andExpect(jsonPath("$.[*].continent").value(hasItem(DEFAULT_CONTINENT.toString())))
+            .andExpect(jsonPath("$.[*].indicatif").value(hasItem(DEFAULT_INDICATIF.toString())))
+            .andExpect(jsonPath("$.[*].source").value(hasItem(DEFAULT_SOURCE.toString())));
+    }
+    
+    @Test
+    @Transactional
+    public void getCountry() throws Exception {
+        // Initialize the database
+        countryRepository.saveAndFlush(country);
+
+        // Get the country
+        restCountryMockMvc.perform(get("/api/countries/{id}", country.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(country.getId().intValue()))
+            .andExpect(jsonPath("$.name").value(DEFAULT_NAME.toString()))
+            .andExpect(jsonPath("$.codeAlpha2").value(DEFAULT_CODE_ALPHA_2.toString()))
+            .andExpect(jsonPath("$.codeAlpha3").value(DEFAULT_CODE_ALPHA_3.toString()))
+            .andExpect(jsonPath("$.continent").value(DEFAULT_CONTINENT.toString()))
+            .andExpect(jsonPath("$.indicatif").value(DEFAULT_INDICATIF.toString()))
+            .andExpect(jsonPath("$.source").value(DEFAULT_SOURCE.toString()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingCountry() throws Exception {
+        // Get the country
+        restCountryMockMvc.perform(get("/api/countries/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateCountry() throws Exception {
+        // Initialize the database
+        countryService.save(country);
+
+        int databaseSizeBeforeUpdate = countryRepository.findAll().size();
+
+        // Update the country
+        Country updatedCountry = countryRepository.findById(country.getId()).get();
+        // Disconnect from session so that the updates on updatedCountry are not directly saved in db
+        em.detach(updatedCountry);
+        updatedCountry
+            .name(UPDATED_NAME)
+            .codeAlpha2(UPDATED_CODE_ALPHA_2)
+            .codeAlpha3(UPDATED_CODE_ALPHA_3)
+            .continent(UPDATED_CONTINENT)
+            .indicatif(UPDATED_INDICATIF)
+            .source(UPDATED_SOURCE);
+
+        restCountryMockMvc.perform(put("/api/countries")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedCountry)))
+            .andExpect(status().isOk());
+
+        // Validate the Country in the database
+        List<Country> countryList = countryRepository.findAll();
+        assertThat(countryList).hasSize(databaseSizeBeforeUpdate);
+        Country testCountry = countryList.get(countryList.size() - 1);
+        assertThat(testCountry.getName()).isEqualTo(UPDATED_NAME);
+        assertThat(testCountry.getCodeAlpha2()).isEqualTo(UPDATED_CODE_ALPHA_2);
+        assertThat(testCountry.getCodeAlpha3()).isEqualTo(UPDATED_CODE_ALPHA_3);
+        assertThat(testCountry.getContinent()).isEqualTo(UPDATED_CONTINENT);
+        assertThat(testCountry.getIndicatif()).isEqualTo(UPDATED_INDICATIF);
+        assertThat(testCountry.getSource()).isEqualTo(UPDATED_SOURCE);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingCountry() throws Exception {
+        int databaseSizeBeforeUpdate = countryRepository.findAll().size();
+
+        // Create the Country
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restCountryMockMvc.perform(put("/api/countries")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(country)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Country in the database
+        List<Country> countryList = countryRepository.findAll();
+        assertThat(countryList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    public void deleteCountry() throws Exception {
+        // Initialize the database
+        countryService.save(country);
+
+        int databaseSizeBeforeDelete = countryRepository.findAll().size();
+
+        // Delete the country
+        restCountryMockMvc.perform(delete("/api/countries/{id}", country.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isNoContent());
+
+        // Validate the database is empty
+        List<Country> countryList = countryRepository.findAll();
+        assertThat(countryList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    @Transactional
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Country.class);
+        Country country1 = new Country();
+        country1.setId(1L);
+        Country country2 = new Country();
+        country2.setId(country1.getId());
+        assertThat(country1).isEqualTo(country2);
+        country2.setId(2L);
+        assertThat(country1).isNotEqualTo(country2);
+        country1.setId(null);
+        assertThat(country1).isNotEqualTo(country2);
+    }
+}
